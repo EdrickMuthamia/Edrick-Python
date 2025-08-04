fb538697f3413582809bd35e7526b6d7bdd71755
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-08-04 15:56:41 +0300

    add  data types

--- Intro/data_type.py
+++ Intro/data_type.py
@@ -1,1 +1,27 @@
 # snake_case 
+
+X=1 #Integer
+print(X) # Print in python
+pi=3.142 #Floating 
+print(pi) # Print in python
+myName="Edrick" # String
+print(myName) # Print in python
+myName1='Edrick'# String
+print(myName1) # Print in python
+is_happy=True#python True/False
+print(is_happy) # Print in python
+
+no_value=None#none is the equivalent to Null in js
+print(no_value) # Print in python
+
+# Advanced Data Types Bracket notation
+my_list=[1,2,3,4,True,None,[]] # List<Array>
+print(my_list) # Print in python
+my_set={1,2,3,4} # Set(unique stuff only ) and Array
+print(my_set) # Print in python
+my_tuple=(1,2,3,4,"Apple") # Tuple my_tuple[4]=Apple
+print(my_tuple) # Print in python
+my_obj={"key":"value","age":23} # Dictionary (key:value pairs) my_obj["key"]
+print(my_obj) # Print in python
+
+## Print All Value : python print() js<console.log>
