47d8ee4fa06a2629f0a85047e1f7a09753b0ee40
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-08-04 16:12:37 +0300

    add updates

--- Intro/data_type.py
+++ Intro/data_type.py
@@ -4,24 +4,34 @@ X=1 #Integer
 print(X) # Print in python
 pi=3.142 #Floating 
 print(pi) # Print in python
+print(type(pi)) # Print type of pi in python
 myName="Edrick" # String
 print(myName) # Print in python
+print(type(myName)) # Print type of myName in python
 myName1='Edrick'# String
 print(myName1) # Print in python
+print(type(myName1)) # Print type of myName1 in python
 is_happy=True#python True/False
 print(is_happy) # Print in python
+print(type(is_happy)) # Print type of is_happy in python
 
 no_value=None#none is the equivalent to Null in js
 print(no_value) # Print in python
+print(type(no_value)) # Print type of no_value in python
 
 # Advanced Data Types Bracket notation
 my_list=[1,2,3,4,True,None,[]] # List<Array>
 print(my_list) # Print in python
+print(my_list[0]) # Accessing first element in the list
+print(type(my_list)) # Print type of my_list in python
 my_set={1,2,3,4} # Set(unique stuff only ) and Array
 print(my_set) # Print in python
+print(type(my_set)) # Print type of my_set in python
 my_tuple=(1,2,3,4,"Apple") # Tuple my_tuple[4]=Apple
-print(my_tuple) # Print in python
+print(my_tuple) # Print in python\
+print(type(my_tuple)) # Print type of my_tuple in python
 my_obj={"key":"value","age":23} # Dictionary (key:value pairs) my_obj["key"]
 print(my_obj) # Print in python
+print(type(my_obj)) # Print type of my_obj in python
 
 ## Print All Value : python print() js<console.log>
