c9cdfdd10bf8234911c7942e7bebf5d02557e827
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-08-05 10:39:00 +0300

    add kwargs

---
+++ concepts/kwargs.py
@@ -0,0 +1,10 @@
+
+## *kwargs -> key word arguments
+## Dictionary{key:value}
+
+def greet (nationality,name ):
+    print("Name is",name)
+    print("From ",nationality)
+
+    ## Kwargs solve the mixup
+greet (nationality="Kenya",name="Edrick")    
