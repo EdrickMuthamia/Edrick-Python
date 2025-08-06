1068d3a7189591d4d2086824d525cca1eafafe01
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-08-06 10:07:56 +0300

    add file

--- decorator/logger.py
+++ decorator/logger.py
@@ -1,3 +1,28 @@
+
+#path
+
+import time
+
+def logger(func):
+    def wrapper(*args,**kwargs):
+        start_time=time.time()
+        func(*args,**kwargs) # will run
+        end_time=time.time()
+        diff=end_time-start_time
+        txt=f"function:{func.__name__} time taken {diff}"
+        f_name="logger.txt"
+        write_file(f_name=f_name,txt=txt)
+    return wrapper
+
 def write_file(f_name,txt):
     with open(f_name,'a') as file:
-        file.write(f"{txt}\n")
+        file.write(f"{txt} \n")
+        
+@logger
+def counter():
+    for n in range(0,100000000):
+        # print(n)
+        pass
+
+
+counter()
